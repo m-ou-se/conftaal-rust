d3b39fb7359f82cab2cf9fe7d56ba8102e858694
Mara Bos <m-ou.se@m-ou.se>
2017-09-27 22:25:26 +0200

    Update test code to test argument list parsing.

--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,13 @@
 extern crate conftaal;
 
-use conftaal::expression::{Expression,OpAndLhs};
+use conftaal::expression::{Expression,OpAndLhs,Literal};
 use conftaal::parse::Parser;
 use conftaal::parse::matcher::{Matcher, MatchMode};
 
 fn reconstruct(e: &Expression) -> String {
 	use Expression::*;
 	use OpAndLhs::*;
+	use Literal::*;
 	match *e {
 		Identifier(a) => a.to_string(),
 		Op{op_source, ref op_and_lhs, ref rhs, parenthesized} => {
@@ -17,13 +18,22 @@ fn reconstruct(e: &Expression) -> String {
 			if parenthesized { s = format!("({})", s); }
 			s
 		},
+		Literal(List{ref elements}) => {
+			let mut s = "list(".to_string();
+			for (i, e) in elements.iter().enumerate() {
+				if i > 0 { s += ", "; }
+				s += &reconstruct(e)[..];
+			}
+			s += ")";
+			s
+		}
 		Literal(_) => "<some literal>".to_string(),
 	}
 }
 
 fn main() {
 	let mut p = Parser{
-		source: "a * (-a.bla + b)"
+		source: "a[x, (w>a)(a, b+c), z + q]" //"a * (-a.bla + b)"
 	};
 
 	println!("Parsing: {}", p.source);
